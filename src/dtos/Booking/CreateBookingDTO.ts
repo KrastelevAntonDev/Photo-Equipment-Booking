6781cf49be67f186d9442c95947e2e434cb381b5
KrastelevAntonDev <krastelev.anton@outlook.com>
2025-09-04 13:41:17 +0300

    refactor: Remove userId from CreateBookingDTO and constructor

--- src/dtos/Booking/CreateBookingDTO.ts
+++ src/dtos/Booking/CreateBookingDTO.ts
@@ -3,10 +3,6 @@ import { IsNotEmpty, IsString, IsDateString, IsArray, IsOptional } from 'class-v
 export class CreateBookingDTO {
   @IsString()
   @IsNotEmpty()
-  userId: string;
-
-  @IsString()
-  @IsNotEmpty()
   roomId: string;
 
   @IsArray()
@@ -21,13 +17,11 @@ export class CreateBookingDTO {
   end: Date;
 
 	constructor(
-		userId: string,
 		roomId: string,
 		equipmentIds: string[],
 		start: Date,
 		end: Date
 	) {
-		this.userId = userId;
 		this.roomId = roomId;
 		this.equipmentIds = equipmentIds;
 		this.start = start;
